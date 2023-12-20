d44cf2cf37929e293034b039458a44765587275a
Sheetal Agrahari <145831862+sheetal-ag@users.noreply.github.com>
2023-12-20 19:21:03 +0545

    Time: 19 ms (26.05%), Space: 18.2 MB (60.38%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+/*int sorting(vector<int>& nums) {
+    sort(nums.begin(),nums.end());
+    for(int i=0;i<nums.size();i++){
+        if(i!=nums[i])
+        return i;
+    }
+    return nums.size();//in case of {0,1} type where nums.size()=2
+    }*/
+ int xorr(vector<int>& nums) {
+      int a=0;
+      int b=0;
+       for(int i=0;i<=nums.size();i++)
+       {    a=a^i;
+        }
+        for(int i=0;i<nums.size();i++)
+       {    
+           b=b^nums[i];
+        }
+      int r= a^b;
+    return r;
+    }
+
+
+    int missingNumber(vector<int>& nums) {
+     return xorr(nums);
+     //return sorting(nums);
+    }
+     
+};
